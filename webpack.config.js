0137c1e87f047db29f6291796c6a8b44b1d0da9d
kalle <kalle.kettunen@outlook.com>
2018-03-25 19:06:26 +0300

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'src/client');
+
+
+
+var config = {
+    entry : APP_DIR+ '/index.js',
+    output :{
+        path :BUILD_DIR,
+        filename : 'bundle.js'
+
+    },
+    module : {
+        rules :[
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test : /\.html$/,
+                use : {
+                    loader : "html-loader",
+                    options : {minimize :true}
+                }
+            }
+        ]
+    },
+    plugins :[
+        new HtmlWebPackPlugin({
+            template : "./src/client/index.html",
+            filename : "./index.html"
+        })
+    ],
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
+
+module.exports = config;
